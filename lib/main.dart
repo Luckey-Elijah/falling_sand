45c38b46187353a5929d410dd59965c1c3356738
Elijah Luckey <luckeyelijah112@gmail.com>
2024-08-20 11:52:10 -0400

    no banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Material(
         child: FallingSand(
           height: 50,
